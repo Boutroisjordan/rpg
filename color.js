34685992e9c13084ccc5961820dc7432949c14f0
Jordan Boutrois <67595577+Boutroisjordan@users.noreply.github.com>
2022-03-01 16:23:39 +0100

    Add files via upload

---
+++ color.js
@@ -0,0 +1,19 @@
+let guerrier = document.getElementById("one");
+let sorcier = document.getElementById("two");
+let archerId = document.getElementById("three");
+
+
+guerrier.addEventListener("mouseover", () => {
+    document.body.style.backgroundColor = "#B22222";
+
+});
+
+sorcier.addEventListener("mouseover", () => {
+    document.body.style.backgroundColor = "#BA55D3";
+
+});
+
+archerId.addEventListener("mouseover", () => {
+    document.body.style.backgroundColor = "#9ACD32";
+
+});
